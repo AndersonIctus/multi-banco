7de67137e419a43643740f42c1e23db25525314d
Anderson Dourado <anderson.ictus@gmail.com>
2021-10-08 10:21:52 -0300

    * Commit Inicial

---
+++ src/main/java/com/multibanco/multiBanco/MultiBancoApplication.java
@@ -0,0 +1,13 @@
+package com.multibanco.multiBanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiBancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multibanco/multiBanco/MultiBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multibanco.multiBanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
